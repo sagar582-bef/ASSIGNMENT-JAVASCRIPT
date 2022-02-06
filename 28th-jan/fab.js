2f3a366b58c977e40356044456cdb074496ee12e
sagar582-bef <sagarswadesh582@gmail.com>
2022-02-06 18:59:48 +0530

    hlw

---
+++ 28th-jan/fab.js
@@ -0,0 +1,15 @@
+var num=prompt("please enter a number");
+num=parseInt(num);
+
+var first=0;
+console.log(first);
+var second=1;
+console.log(second);
+for(var i=3;i<=num;i++ ){
+    var third=first+second;
+    console.log(third);
+    first=second;
+  
+    second=third; 
+
+}
