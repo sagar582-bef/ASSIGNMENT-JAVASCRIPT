fa5845fe5b463755b16ca5cd5d394ebe5bdf8d95
sagar582-bef <sagarswadesh582@gmail.com>
2022-02-06 18:40:38 +0530

    hjh

---
+++ 27th-jan/27th-jan.js
@@ -0,0 +1,15 @@
+var r=prompt("size of array");
+var arr = [];
+for(var i=0;i<r;i++){
+   // arr[i]=new Array(r);
+arr[i]= prompt("Enter random number");
+}
+console.log(arr[i]);
+var max=arr[0];
+for(var i=0;i<r;i++){
+    if(max<arr[i]){
+        max=arr[i];
+    }
+}
+alert(max);
+console.log( max);
