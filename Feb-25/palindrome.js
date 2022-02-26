f69078afb4fff6612adc74354eddbab3bbb3bbe1
sagar582-bef <sagarswadesh582@gmail.com>
2022-02-26 20:32:05 +0530

    assignment1 complete

---
+++ Feb-25/palindrome.js
@@ -0,0 +1,20 @@
+function stringPalindrome(str){
+    var len=string.length;
+    for(i=0;i<len/2;i++){
+        if(string[i] !== string[len-1-i]){
+        
+         
+      return document.write("FALSE")
+        }
+       
+      
+    }
+    document.write("TRUE");
+
+}
+
+
+ var string=prompt("Enter only charecter or number");
+
+ stringPalindrome(string);
+
